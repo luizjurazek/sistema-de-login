8493ab408dd0b9d85ae0c3116f936859abbc3def
Luiz Jurazek <luizjurazek@gmail.com>
2023-02-24 22:01:20 -0300

    Sistema de login

---
+++ app.js
@@ -0,0 +1,161 @@
+// Importando dependencias
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+
+const app = express()
+
+//Config JSON response
+app.use(express.json())
+
+// Models 
+const User = require('./models/User')
+
+// Public route
+app.get('/', (req, res) => {
+    res.status(200).json({
+        msg: "Bem vindo a minha API!"
+    })
+})
+
+// Private route
+app.get("/user/:id", checkToken, async (req, res) => {
+    const id = req.params.id
+
+    // Check if user exists
+    // Checar problema de id não encontrado
+    const user = await User.findById(id, '-password')
+
+    if(!user){
+        return res.status(404).json({msg: 'Usuário não encontrado!'})
+    }
+
+    res.status(200).json({ user })
+
+    
+})
+
+// Function to check token
+function checkToken(req, res, next){
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(" ")[1]
+    console.log(token)
+
+    if(!token){
+        return res.status(401).json({msg: 'Acesso negado!'})
+    }
+
+    try {
+        const secret = process.env.SECRET
+        jwt.verify(token, secret)
+
+        next()
+    } catch(err){
+        res.status(400).json({msg: "Token inválido!"})
+    }
+}
+
+// Register user 
+app.post('/auth/register', async(req, res) => {
+
+    const { name, email, password, confirmpassword } = req.body
+
+    // Validations
+    if(!name){
+        return res.status(422).json({msg: "O nome é obrigatório!"})
+    }
+    if(!email){
+        return res.status(422).json({msg: "O email é obrigatório!"})
+    }
+    if(!password){
+        return res.status(422).json({msg: "A senha é obrigatória!"})
+    }
+
+    if(password !== confirmpassword){
+        return res.status(422).json({msg: "As senhas são diferentes!"})
+    }
+
+    // Check if user exists
+    const userExists = await User.findOne({email: email})
+
+    if(userExists){
+        return res.status(422).json({msg: "Email já cadastrado!"})
+    }
+
+    // Create password
+    const salt = await bcrypt.genSalt(12)
+    const passwordHash = await bcrypt.hash(password, salt)
+
+    // Create user
+    const user = new User({
+        name,
+        email,
+        password: passwordHash
+    })
+
+    try {
+        await user.save()
+        res.status(201).json({msg: "Usuário criado com sucesso!"})
+
+    } catch(err){
+        console.log(err)
+        res.status(500).json({msg: "Erro no servidor, tente novamente mais tarde!"})
+    }
+})
+
+// Login User
+app.post('/auth/user', async (req, res) => {
+    const { email, password } = req.body
+
+    // validations
+    if(!email){
+        return res.status(422).json({msg: "O email é obrigatório!"})
+    }
+    if(!password){
+        return res.status(422).json({msg: "O nome é obrigatório!"})
+    }
+
+    //Check if user exists
+    const user = await User.findOne({email: email})
+
+    if(!user){
+        return res.status(404).json({msg: "Usuário não encontrado!"})
+    }
+
+    // check if password match 
+    const checkPassword = await bcrypt.compare(password, user.password)
+
+    if(!checkPassword){
+        return res.status(422).json({msg: "Senha inválida!"})
+    }
+
+    try{
+        const secret = process.env.secret
+        const token = jwt.sign(
+            {
+                id: user._id
+            }, 
+            secret,
+        )
+        res.status(200).json({msg: "Autenticação realizada com sucesso!", token})
+    } catch (err){
+        console.log(err)
+        res.status(500).json({msg: "Erro no servidor, tente novamente mais tarde!"})
+    }
+ 
+})
+
+// Credenciais
+const dbUser = process.env.DB_USER
+const dbPassword = process.env.DB_PASS
+
+mongoose.set('strictQuery', true); 
+mongoose.connect(`mongodb+srv://${dbUser}:${dbPassword}@login.1hzc0ff.mongodb.net/?retryWrites=true&w=majority`).then(() => {
+    app.listen(3000)
+    console.log("Conectado ao banco!")
+}).catch((err) => {console.log(err)})
+
+
+
